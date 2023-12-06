39f0437c9c0d537d71ccf4e6accfbb567cdaa248
Lewis Kai Ho Wong (k22027157) <k22027157@kcl.ac.uk>
2023-12-06 00:56:02 +0000

    update

--- things/forms.py
+++ things/forms.py
@@ -16,10 +16,13 @@ class ThingForm(forms.Form):
         entered_desc = self.cleaned_data.get("description")
         entered_quantity = self.cleaned_data.get("quantity")
 
+        if entered_name is not None:
+            self.add_error("name can't be empty")
+
         if len(entered_name) > 35:
             self.add_error("name can't be more than 35 characters")
 
-        if len(entered_desc) > 120:
+        if entered_desc is not None and len(entered_desc) > 120:
             self.add_error("description can't be more than 120 characters")
 
         if entered_quantity < 0 or entered_quantity > 50:
