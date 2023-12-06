6a426d22713f86320b9f195ef7ac4c23e1ce35a6
Lewis Kai Ho Wong (k22027157) <k22027157@kcl.ac.uk>
2023-12-06 01:15:55 +0000

    update

--- things/forms.py
+++ things/forms.py
@@ -6,9 +6,10 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class ThingForm(forms.Form):
     name = forms.CharField(label = "name", max_length=35)
-    description = forms.CharField(label = "description", max_length=120)
+    description = forms.CharField(label = "description", max_length=120, widget=forms.Textarea)
     quantity = forms.IntegerField(label = "quantity",
-                                  validators=[MinValueValidator(0),MaxValueValidator(50)])
+                                  validators=[MinValueValidator(0),MaxValueValidator(50)],
+                                  widget=forms.NumberInput)
 
     def clean(self):
         super().clean()
@@ -16,17 +17,12 @@ class ThingForm(forms.Form):
         entered_desc = self.cleaned_data.get("description")
         entered_quantity = self.cleaned_data.get("quantity")
 
-        if entered_name is not None:
-            self.add_error("name can't be empty")
-
-        if len(entered_name) > 35:
-            self.add_error("name can't be more than 35 characters")
+        if entered_name is not None and len(entered_name) > 35:
+            self.add_error("name","name can't be more than 35 characters")
 
         if entered_desc is not None and len(entered_desc) > 120:
-            self.add_error("description can't be more than 120 characters")
+            self.add_error("description","description can't be more than 120 characters")
 
-        if entered_quantity < 0 or entered_quantity > 50:
-            self.add_error("quantity must be between 0 and 50")
+        if entered_quantity is not None and (entered_quantity < 0 or entered_quantity > 50):
+            self.add_error("quantity","quantity must be between 0 and 50")
 
-    def save(self):
-        super().save()
